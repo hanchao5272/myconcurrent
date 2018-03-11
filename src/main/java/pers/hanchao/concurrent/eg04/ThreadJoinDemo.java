77acd3d5c770a4197c66a05973a0e36e8be0d017
hanchao5272 <89298141@qq.com>
2018-03-11 16:15:44 +0800

    java并发04:线程的基本方法(下2:join()方法)
    1.在一个线程a中,执行b.join,表示:需要等到线程b结束之后,才能结束线程a

---
+++ src/main/java/pers/hanchao/concurrent/eg04/ThreadJoinDemo.java
@@ -0,0 +1,72 @@
+package pers.hanchao.concurrent.eg04;
+
+import org.apache.log4j.Logger;
+
+/**
+ * <p>java线程基本方法：join</p>
+ *
+ * @author hanchao 2018/3/11 16:03
+ **/
+public class ThreadJoinDemo {
+    private static final Logger LOGGER = Logger.getLogger(ThreadJoinDemo.class);
+
+    /**
+     * <p>join：让当前线程等待指定的线程结束</p>
+     *
+     * @author hanchao 2018/3/11 16:04
+     **/
+    public static void main(String[] args) throws InterruptedException {
+        //不加join
+        LOGGER.info("==========不使用t.join()方法==========");
+        new Thread(() -> {
+            LOGGER.info("[线程1]开始运行...");
+            Thread otherThread = new Thread(() -> {
+                LOGGER.info("[线程2]开始运行...");
+                try {
+                    Thread.sleep(200);//线程2运行200毫秒
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                LOGGER.info("[线程2]运行结束");
+            });
+            //启动线程2
+            otherThread.start();
+            try {
+                Thread.sleep(100);//线程1运行100毫秒
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LOGGER.info("[线程1]运行结束");
+        }).start();//启动线程1
+
+        //使用t.join()方法
+        Thread.sleep(1000);//等待线程1和线程2都有运行完
+        System.out.println();
+        LOGGER.info("==========使用t.join()方法==========");
+        new Thread(() -> {
+            LOGGER.info("[线程11]开始运行...");
+            Thread otherThread = new Thread(() -> {
+                LOGGER.info("[线程22]开始运行...");
+                try {
+                    Thread.sleep(200);//线程22运行200毫秒
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                LOGGER.info("[线程22]运行结束");
+            });
+            otherThread.start();//启动线程22
+            try {
+                //在线程11中，加入线程22-->需要等到线程22结束之后才能结束线程11
+                otherThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                Thread.sleep(100);//线程11运行100毫秒
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LOGGER.info("[线程11]运行结束");
+        }).start();//启动线程11
+    }
+}

c875359b7ab3ba2f6b1fe2496c3de80bcbac0823
hanchao5272 <89298141@qq.com>
2018-03-18 14:44:49 +0800

    Lock接口01:Lock的基本方法
    1.lock.lock():轮询去获取锁(不可中断):轮询去获取锁,直到获取锁.(不可中断)
    2.lock.tryLock():尝试去获取锁(一次),如果获取到，则持有锁返回true;反之返回false.(不可中断)
    3.lock.tryLock(long,TimeUnit):尝试去获取锁(一定时间内),如果获取到，则持有锁返回true;如果时间超时还未获取锁，则返回false.(不可中断)
    4.lock.lockInterruptibly():可中断的轮询去获取锁:轮询去获取锁,直到获取锁或者被thread.interrupt()中断
    5.lock.unlock():释放锁，一般放在finally块中
    6.lock.newCondition():获取条件对象，后续代码再进行学习

---
+++ src/main/java/pers/hanchao/concurrent/eg07/LockDemo.java
@@ -0,0 +1,137 @@
+package pers.hanchao.concurrent.eg07;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * <p>Lock接口-方法学习</p>
+ *
+ * @author hanchao 2018/3/18 13:58
+ **/
+public class LockDemo {
+
+    //定义一个非公平的锁
+    private static Lock lock = new ReentrantLock(false);
+
+    /**
+     * <p>Lock接口方法学习</p>
+     *
+     * @author hanchao 2018/3/18 13:54
+     **/
+    public static void main(String[] args) throws InterruptedException {
+        //线程0一直持有锁5000毫秒
+        new Thread(() -> {
+            try {
+                System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取锁");
+                lock.lock();
+                System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {//在finally代码块中是否锁
+                try {
+                    lock.unlock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
+                } catch (IllegalMonitorStateException e) {
+                    //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到锁，线程结束.");
+                }
+            }
+        }).start();
+        Thread.sleep(10);
+        //线程1通过lock.lock()持续去尝试获取锁
+        new Thread(() -> {
+            try {
+                System.out.println("线程[" + Thread.currentThread().getName() + "]通过lock.lock()持续去尝试获取锁");
+                lock.lock();
+                System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {//在finally代码块中是否锁
+                try {
+                    lock.unlock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
+                } catch (IllegalMonitorStateException e) {
+                    //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到锁，线程结束.");
+                }
+            }
+        }).start();
+        //线程2通过lock.tryLock()尝试去获取一次锁
+        new Thread(() -> {
+            System.out.println("线程[" + Thread.currentThread().getName() + "]通过lock.tryLock()尝试去获取一次锁");
+            if (lock.tryLock()) {
+                try {
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        lock.unlock();
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
+                    } catch (IllegalMonitorStateException e) {
+                        //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到锁，线程结束.");
+                    }
+                }
+            } else {
+                System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取锁失败，不再等待.");
+            }
+        }).start();
+        //线程3通过lock.tryLock(long,TimeUnit)尝试在一定时间内去获取锁
+        new Thread(() -> {
+            System.out.println("线程[" + Thread.currentThread().getName() + "]通过lock.tryLock(long,TimeUnit)尝试在一定时间内去获取锁");
+            try {
+                if (lock.tryLock(2, TimeUnit.SECONDS)) {
+                    try {
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            lock.unlock();
+                            System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
+                        } catch (IllegalMonitorStateException e) {
+                            //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
+                            System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到锁，线程结束.");
+                        }
+                    }
+                } else {
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]在指定时间内没有获取到锁，不再等待.");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        //线程4通过lock.lockInterruptibly()尝试可中断的去获取锁
+        Thread thread5 = new Thread(() -> {
+            try {
+                System.out.println("线程[" + Thread.currentThread().getName() + "]通过lock.lockInterruptibly()尝试可中断的去获取锁");
+                lock.lockInterruptibly();
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } catch (InterruptedException e) {
+                //e.printStackTrace(); 被中断时会产生的意料之中的错误，无需打印
+                System.out.println("线程[" + Thread.currentThread().getName() + "]被thread.interrupt()中断，不在尝试去获取锁");
+            } finally {
+                try {
+                    lock.unlock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
+                } catch (IllegalMonitorStateException e) {
+                    //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到锁，线程结束.");
+                }
+            }
+        });
+        thread5.start();
+        Thread.sleep(3000);
+        thread5.interrupt();
+    }
+}
---
+++ src/main/java/pers/hanchao/concurrent/eg07/示例07-Lock.md
@@ -0,0 +1,63 @@
+# 示例07-Lock
+
+http://www.cnblogs.com/dolphin0520/p/3923167.html
+
+## 锁和同步块之间的主要区别如下：
+
+- 公平锁:
+    公平锁即尽量以请求锁的顺序来获取锁。比如同是有多个线程在等待一个锁，当这个锁被释放时，等待时间最久的线程（最先请求的线程）会获得该所，这种就是公平锁。
+    非公平锁即无法保证锁的获取是按照请求锁的顺序进行的。这样就可能导致某个或者一些线程永远获取不到锁。
+    在Java中，synchronized就是非公平锁，它无法保证等待的线程获取锁的顺序。
+    而对于ReentrantLock和ReentrantReadWriteLock，它默认情况下是非公平锁，但是可以设置为公平锁。
+- 可定时锁：
+    可定时锁,顾名思义，就是可以指定时间进行加锁的锁。
+    synchronized不是可定时锁，会一直尝试加锁，直至获取锁。
+    Lock是可定时锁,可以指定尝试加锁时间，超时则不再尝试获取锁。
+- 可中断锁：
+    可中断锁：顾名思义，就是可以相应中断的锁。
+    在Java中，synchronized就不是可中断锁，而Lock是可中断锁。
+    如果某一线程A正在执行锁中的代码，另一线程B正在等待获取该锁，可能由于等待时间过长，线程B不想等待了，想先处理其他事情，我们可以让它中断自己或者在别的线程中中断它，这种就是可中断锁。
+    在前面演示lockInterruptibly()的用法时已经体现了Lock的可中断性。
+- 读写锁：
+    读写锁将对一个资源（比如文件）的访问分成了2个锁，一个读锁和一个写锁。
+    正因为有了读写锁，才使得多个线程之间的读操作不会发生冲突。
+    ReadWriteLock就是读写锁，它是一个接口，ReentrantReadWriteLock实现了这个接口。
+    可以通过readLock()获取读锁，通过writeLock()获取写锁。
+- 单一方法：
+    同步块必须完全包含在单个方法中，
+    而Lock接口的方法lock()和unlock()可以以不同的方式调用。
+
+## Lock接口的主要方法：
+
+1.lock.lock():轮询去获取锁(不可中断):轮询去获取锁,直到获取锁.(不可中断)
+2.lock.tryLock():尝试去获取锁(一次),如果获取到，则持有锁返回true;反之返回false.(不可中断)
+3.lock.tryLock(long,TimeUnit):尝试去获取锁(一定时间内),如果获取到，则持有锁返回true;如果时间超时还未获取锁，则返回false.(不可中断)
+4.lock.lockInterruptibly():可中断的轮询去获取锁:轮询去获取锁,直到获取锁或者被thread.interrupt()中断
+5.lock.unlock():释放锁，一般放在finally块中
+6.lock.newCondition():获取条件对象，后续代码再进行学习
+
+## ReentrantLock 可重入锁
+ReentrantLock是唯一实现了Lock接口的类，并且ReentrantLock提供了更多的方法。
+java.util.concurrent.locks.ReadWriteLock接口允许一次读取多个线程，但一次只能写入一个线程。
+
+读锁 - 如果没有线程锁定ReadWriteLock进行写入，则多线程可以访问读锁。
+写锁 - 如果没有线程正在读或写，那么一个线程可以访问写锁。
+
+写读、读写、写写互斥，读读不互斥。
+
+### ReentrantLock的方法
+1	public Lock readLock()	返回用于读的锁。
+2	public Lock writeLock()	返回用于写的锁。
+
+## Condition接口
+
+|序号|方法名称|方法简述|描述|
+|---|---|---|---|
+|1|public void await()|等待|使当前线程等待，直到被唤醒或被中断。|
+|2|public boolean await(long time, TimeUnit unit)|定时等待|使当前线程等待，直到被唤醒、被中断或等待时间超时。|
+|3|public long awaitNanos(long nanosTimeout)|纳秒定时等待|使当前线程等待，直到被唤醒、被中断或等待时间(纳秒)超时。|
+|4|public long awaitUninterruptibly()|可中断等待|使当前线程等待，直到被唤醒。|
+|5|public long awaitUntil(Date deadline)|等待|使当前线程等待，直到被唤醒、被中断或最后期限到达。|
+|6|public void signal()|单个唤醒|唤醒一个等待线程。|
+|7|public void signalAll()|全部等待|唤醒所有等待线程。|
+

6d331046c9c94fd218e95320471a29ef87bd0ad8
hanchao5272 <89298141@qq.com>
2018-03-24 18:54:38 +0800

    unLock()操作的代码优化

--- src/main/java/pers/hanchao/concurrent/eg07/LockDemo.java
+++ src/main/java/pers/hanchao/concurrent/eg07/LockDemo.java
@@ -22,60 +22,45 @@ public class LockDemo {
     public static void main(String[] args) throws InterruptedException {
         //线程0一直持有锁5000毫秒
         new Thread(() -> {
+            System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取锁");
+            lock.lock();
+            System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
             try {
-                System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取锁");
-                lock.lock();
-                System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {//在finally代码块中是否锁
-                try {
-                    lock.unlock();
-                    System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
-                } catch (IllegalMonitorStateException e) {
-                    //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                    System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到锁，线程结束.");
-                }
+                lock.unlock();
+                System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
             }
         }).start();
         Thread.sleep(10);
         //线程1通过lock.lock()持续去尝试获取锁
         new Thread(() -> {
+            System.out.println("线程[" + Thread.currentThread().getName() + "]通过lock.lock()持续去尝试获取锁");
+            lock.lock();
+            System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
             try {
-                System.out.println("线程[" + Thread.currentThread().getName() + "]通过lock.lock()持续去尝试获取锁");
-                lock.lock();
-                System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
                 Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {//在finally代码块中是否锁
-                try {
-                    lock.unlock();
-                    System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
-                } catch (IllegalMonitorStateException e) {
-                    //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                    System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到锁，线程结束.");
-                }
+                lock.unlock();
+                System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
             }
         }).start();
         //线程2通过lock.tryLock()尝试去获取一次锁
         new Thread(() -> {
             System.out.println("线程[" + Thread.currentThread().getName() + "]通过lock.tryLock()尝试去获取一次锁");
             if (lock.tryLock()) {
+                System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
                 try {
-                    System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
                     Thread.sleep(10);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
-                    try {
-                        lock.unlock();
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
-                    } catch (IllegalMonitorStateException e) {
-                        //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到锁，线程结束.");
-                    }
+                    lock.unlock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
                 }
             } else {
                 System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取锁失败，不再等待.");
@@ -86,29 +71,25 @@ public class LockDemo {
             System.out.println("线程[" + Thread.currentThread().getName() + "]通过lock.tryLock(long,TimeUnit)尝试在一定时间内去获取锁");
             try {
                 if (lock.tryLock(2, TimeUnit.SECONDS)) {
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
                     try {
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]获取了锁...");
-                        Thread.sleep(5000);
+                        Thread.sleep(10);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     } finally {
-                        try {
-                            lock.unlock();
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
-                        } catch (IllegalMonitorStateException e) {
-                            //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到锁，线程结束.");
-                        }
+                        lock.unlock();
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
                     }
                 } else {
                     System.out.println("线程[" + Thread.currentThread().getName() + "]在指定时间内没有获取到锁，不再等待.");
                 }
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                //e.printStackTrace(); 被中断时会产生的意料之中的错误，无需打印
+                System.out.println("线程[" + Thread.currentThread().getName() + "]被thread.interrupt()中断，不在尝试去获取锁");
             }
         }).start();
         //线程4通过lock.lockInterruptibly()尝试可中断的去获取锁
-        Thread thread5 = new Thread(() -> {
+        Thread thread4 = new Thread(() -> {
             try {
                 System.out.println("线程[" + Thread.currentThread().getName() + "]通过lock.lockInterruptibly()尝试可中断的去获取锁");
                 lock.lockInterruptibly();
@@ -116,22 +97,17 @@ public class LockDemo {
                     Thread.sleep(10);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                }finally {
+                    lock.unlock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
                 }
             } catch (InterruptedException e) {
                 //e.printStackTrace(); 被中断时会产生的意料之中的错误，无需打印
                 System.out.println("线程[" + Thread.currentThread().getName() + "]被thread.interrupt()中断，不在尝试去获取锁");
-            } finally {
-                try {
-                    lock.unlock();
-                    System.out.println("线程[" + Thread.currentThread().getName() + "]释放了锁..");
-                } catch (IllegalMonitorStateException e) {
-                    //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                    System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到锁，线程结束.");
-                }
             }
         });
-        thread5.start();
+        thread4.start();
         Thread.sleep(3000);
-        thread5.interrupt();
+        thread4.interrupt();
     }
 }

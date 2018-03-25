389d331790ba88aece1933d1ecc199542223e9e8
hanchao5272 <89298141@qq.com>
2018-03-25 13:36:38 +0800

    读写锁示例优化

--- src/main/java/pers/hanchao/concurrent/eg07/ReadWriteLockDemo.java
+++ src/main/java/pers/hanchao/concurrent/eg07/ReadWriteLockDemo.java
@@ -17,7 +17,7 @@ public class ReadWriteLockDemo {
      *
      * @author hanchao 2018/3/18 15:41
      **/
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /**
          * 0 写写互斥
          * 1 写读互斥
@@ -31,40 +31,30 @@ public class ReadWriteLockDemo {
                 Lock writeLock = lock.writeLock();
                 //写
                 new Thread(() -> {
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取写锁...");
+                    writeLock.lock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]获取了写锁.");
                     try {
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取写锁");
-                        writeLock.lock();
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]获取了写锁...");
-                        Thread.sleep(1000);
+                        Thread.sleep(2000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     } finally {//在finally代码块中是否锁
-                        try {
-                            writeLock.unlock();
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]释放了写锁..");
-                        } catch (IllegalMonitorStateException e) {
-                            //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到写锁，线程结束.");
-                        }
+                        writeLock.unlock();
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]释放了写锁.");
                     }
                 }).start();
                 //写
                 new Thread(() -> {
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取写锁...");
+                    writeLock.lock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]获取了写锁.");
                     try {
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取写锁");
-                        writeLock.lock();
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]获取了写锁...");
-                        Thread.sleep(1000);
+                        Thread.sleep(1800);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     } finally {//在finally代码块中是否锁
-                        try {
-                            writeLock.unlock();
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]释放了写锁..");
-                        } catch (IllegalMonitorStateException e) {
-                            //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到写锁，线程结束.");
-                        }
+                        writeLock.unlock();
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]释放了写锁.");
                     }
                 }).start();
                 break;
@@ -74,40 +64,30 @@ public class ReadWriteLockDemo {
                 Lock readLock1 = lock.readLock();
                 //写
                 new Thread(() -> {
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取写锁...");
+                    writeLock1.lock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]获取了写锁.");
                     try {
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取写锁");
-                        writeLock1.lock();
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]获取了写锁...");
-                        Thread.sleep(1000);
+                        Thread.sleep(1900);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     } finally {//在finally代码块中是否锁
-                        try {
-                            writeLock1.unlock();
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]释放了写锁..");
-                        } catch (IllegalMonitorStateException e) {
-                            //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到写锁，线程结束.");
-                        }
+                        writeLock1.unlock();
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]释放了写锁.");
                     }
                 }).start();
                 //读
                 new Thread(() -> {
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取读锁...");
+                    readLock1.lock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]获取了读锁.");
                     try {
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取读锁");
-                        readLock1.lock();
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]获取了读锁...");
-                        Thread.sleep(1000);
+                        Thread.sleep(2100);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     } finally {//在finally代码块中是否锁
-                        try {
-                            readLock1.unlock();
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]释放了读锁..");
-                        } catch (IllegalMonitorStateException e) {
-                            //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到读锁，线程结束.");
-                        }
+                        readLock1.unlock();
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]释放了读锁.");
                     }
                 }).start();
                 break;
@@ -117,40 +97,30 @@ public class ReadWriteLockDemo {
                 Lock readLock2 = lock.readLock();
                 //读
                 new Thread(() -> {
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取读锁...");
+                    readLock2.lock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]获取了读锁.");
                     try {
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取读锁");
-                        readLock2.lock();
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]获取了读锁...");
-                        Thread.sleep(1000);
+                        Thread.sleep(2200);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     } finally {//在finally代码块中是否锁
-                        try {
-                            readLock2.unlock();
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]释放了读锁..");
-                        } catch (IllegalMonitorStateException e) {
-                            //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到读锁，线程结束.");
-                        }
+                        readLock2.unlock();
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]释放了读锁.");
                     }
                 }).start();
                 //写
                 new Thread(() -> {
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取写锁...");
+                    writeLock2.lock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]获取了写锁.");
                     try {
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取写锁");
-                        writeLock2.lock();
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]获取了写锁...");
-                        Thread.sleep(1000);
+                        Thread.sleep(1700);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     } finally {//在finally代码块中是否锁
-                        try {
-                            writeLock2.unlock();
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]释放了写锁..");
-                        } catch (IllegalMonitorStateException e) {
-                            //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到写锁，线程结束.");
-                        }
+                        writeLock2.unlock();
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]释放了写锁.");
                     }
                 }).start();
                 break;
@@ -159,45 +129,37 @@ public class ReadWriteLockDemo {
                 Lock readLock3 = lock.readLock();
                 //读
                 new Thread(() -> {
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取读锁...");
+                    readLock3.lock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]获取了读锁.");
                     try {
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取读锁");
-                        readLock3.lock();
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]获取了读锁...");
-                        Thread.sleep(1000);
+                        Thread.sleep(1800);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     } finally {//在finally代码块中是否锁
-                        try {
-                            readLock3.unlock();
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]释放了读锁..");
-                        } catch (IllegalMonitorStateException e) {
-                            //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到读锁，线程结束.");
-                        }
+                        readLock3.unlock();
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]释放了读锁.");
                     }
                 }).start();
                 //读
                 new Thread(() -> {
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取读锁...");
+                    readLock3.lock();
+                    System.out.println("线程[" + Thread.currentThread().getName() + "]获取了读锁.");
                     try {
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]尝试获取读锁");
-                        readLock3.lock();
-                        System.out.println("线程[" + Thread.currentThread().getName() + "]获取了读锁...");
-                        Thread.sleep(1000);
+                        Thread.sleep(1600);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     } finally {//在finally代码块中是否锁
-                        try {
-                            readLock3.unlock();
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]释放了读锁..");
-                        } catch (IllegalMonitorStateException e) {
-                            //e.printStackTrace(); 没有获取锁，尝试去解锁，意料之中的错误，无需打印
-                            System.out.println("线程[" + Thread.currentThread().getName() + "]没有获取到读锁，线程结束.");
-                        }
+                        readLock3.unlock();
+                        System.out.println("线程[" + Thread.currentThread().getName() + "]释放了读锁.");
                     }
                 }).start();
                 break;
             default:
                 break;
         }
+        Thread.sleep(500);
+        System.out.println("============================");
     }
 }
